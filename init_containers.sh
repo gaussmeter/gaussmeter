7e9e703c0429d79bef12807b722048cfa7116694
Leland Sindt <leland@sway.org>
2020-12-27 11:26:30 -0600

    fix teslamater -> docker command

--- init_containers.sh
+++ init_containers.sh
@@ -1,4 +1,4 @@
 set -x
 docker network create gaussmeter
 docker container run --detach --restart always --network gaussmeter --name lumen --privileged  gaussmeter/lumen:latest
-docker container run --detach --restart always --network gaussmeter --name teslamater -env LUMEN_HOST=http://lumen:9000/lumen -env MQTT_HOST=ws://192.168.1.51:9001 gaussmeter/teslamater:latest
+docker container run --detach --restart always --network gaussmeter --name teslamater --env LUMEN_HOST=http://lumen:9000/lumen --env MQTT_HOST=ws://192.168.1.51:9001 gaussmeter/teslamater:latest
